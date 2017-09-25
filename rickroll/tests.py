42ac2d38756d59794232100aa91dc43b965e2245
kjwon15 <kjwonmail@gmail.com>
2017-09-26 02:44:46 +0900

    Raise 404 when object is not found or invalid id

--- rickroll/tests.py
+++ rickroll/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+from .models import Link
+
+
+class RickrollTest(TestCase):
+
+    def setUp(self):
+        Link.objects.create(
+            id=1,
+            title='test title',
+            description='test description',
+            destination='http://test.com',
+        )
+
+    def test_id_str(self):
+        link = Link.objects.get(id=1)
+        self.assertIsNotNone(link.id_str)
+
+    def test_attributes(self):
+        link = Link.objects.get(id=1)
+        self.assertEqual(link.title, 'test title')
+        self.assertEqual(link.description, 'test description')
+        self.assertEqual(link.destination, 'http://test.com')
+
+    def test_preview(self):
+        link = Link.objects.get(id=1)
+        url = reverse('preview', args=[link.id_str])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(link.destination, response.content.decode())
+        self.assertIn(link.title, response.content.decode())
+        self.assertIn(link.description, response.content.decode())
+
+    def test_link_view(self):
+        link = Link.objects.get(id=1)
+        url = reverse('link_view', args=[link.id_str])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(link.destination, response.content.decode())
+        self.assertIn(link.title, response.content.decode())
+        self.assertIn(link.description, response.content.decode())
+
+    def test_404(self):
+        url_preview = reverse('preview', args=['00'])
+        response = self.client.get(url_preview)
+        self.assertEqual(response.status_code, 404)
--- rickroll/views.py
+++ rickroll/views.py
@@ -1,5 +1,6 @@
 import base58
-from django.shortcuts import redirect, render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import LinkForm
 from .models import Link
@@ -20,17 +21,22 @@ def create_new(request):
 
 
 def link_view(request, id_str):
-
-    id_int = base58.b58decode_int(id_str)
-    link = Link.objects.get(id=id_int)
+    try:
+        id_int = base58.b58decode_int(id_str)
+    except ValueError:
+        raise Http404
+    link = get_object_or_404(Link, id=id_int)
     return render(request, 'link_view.html', {
         'link': link,
     })
 
 
 def preview(request, id_str):
-    id_int = base58.b58decode_int(id_str)
-    link = Link.objects.get(id=id_int)
+    try:
+        id_int = base58.b58decode_int(id_str)
+    except ValueError:
+        raise Http404
+    link = get_object_or_404(Link, id=id_int)
     return render(request, 'preview.html', {
         'link': link,
     })
