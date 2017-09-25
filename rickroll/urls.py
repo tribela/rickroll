496a94fe6f1c10a179771d4746f6df34a15fec5e
kjwon15 <kjwonmail@gmail.com>
2017-09-26 04:57:07 +0900

    Fix regex urlpatterns

--- rickroll/urls.py
+++ rickroll/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.create_new, name='create_new'),
     url(r'^(?P<id_str>.*)/preview', views.preview, name='preview'),
-    url(r'^(?P<id_str>.*)', views.link_view, name='link_view'),
+    url(r'^(?P<id_str>[^/]+)/?$', views.link_view, name='link_view'),
 ]
